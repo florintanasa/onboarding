602f60b309fb4d627822cdb0422960286d0e200a
Florin Tanasa <florin.tanasa@gmail.com>
2026-06-09 12:34:24 +0300

    Update DemoDataInitializer.java

--- src/main/java/com/company/onboarding/listener/DemoDataInitializer.java
+++ src/main/java/com/company/onboarding/listener/DemoDataInitializer.java
@@ -26,26 +26,46 @@ public class DemoDataInitializer {
 			return;
 		}
 
-		// 1. Populating the Steps directory.
+		// 1. Populating the STEPS table
 		createStep("Safety briefing", 1, 10);
 		createStep("Fill in profile", 1, 20);
-		createStep("Check functions", 2, 30);
+		createStep("Check all functions", 2, 30);
+		createStep("Information security training", 3, 40);
+		createStep("Internal procedures studying", 5, 50);
 
-		// 2. Populating the Status directory.
-		createStatus("ÎN_CURS");
-		createStatus("FINALIZAT");
+		// 2. Populating the DEPARTMENT table
+		createDepartment("Human Resources");
+		createDepartment("Marketing");
+		createDepartment("Operations");
+		createDepartment("Finance");
 
-		// 3. Creating a test user with a securely encrypted BCrypt password.
-		User user = dm.create(User.class);
-		user.setUsername("alice");
-		user.setPassword(encoder.encode("1"));
-		user.setFirstName("Alice");
-		user.setLastName("Brown");
-		dm.save(user);
+		// 3. Populating the STATUS table
+		createStatus("IN PROGRESS");
+		createStatus("COMPLETED");
+
+		// 3. Populating the User table
+		createUser("alice", "1", "Alice", "Brown", "alice.brown@company.com");
 
 		System.out.println(
 			"✨ The database was automatically populated with test data!"
 		);
+	}
+
+	private void createUser(
+		String username,
+		String password,
+		String firstName,
+		String lastName,
+		String email
+	) {
+		User user = dm.create(User.class);
+		user.setUsername(username);
+		user.setPassword(encoder.encode(password));
+		user.setFirstName(firstName);
+		user.setLastName(lastName);
+		user.setEmail(email);
+
+		dm.save(user);
 	}
 
 	private void createStep(String name, Integer duration, Integer sortValue) {
@@ -60,5 +80,11 @@ public class DemoDataInitializer {
 		OnboardingStatus status = dm.create(OnboardingStatus.class);
 		status.setName(name);
 		dm.save(status);
+	}
+
+	private void createDepartment(String name) {
+		Department department = dm.create(Department.class);
+		department.setName(name);
+		dm.save(department);
 	}
 }
